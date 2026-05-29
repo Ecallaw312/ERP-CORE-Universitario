933b41cb40000de40bc6c04847fe1d7323d09beb
Wallace Souza <wallacesouza.130903@gmail.com>
2026-05-29 20:59:55 -0300

    RefreshToken-LoginP/Testes-ConferirToken

--- app/core/dependencia.py
+++ app/core/dependencia.py
@@ -1,5 +1,9 @@
 from app.core.database import db
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
+from jose import JWTError, jwt
+from app.core.security import SECRET_KEY, ALGORITHM, oauth2_scheme
+from fastapi import Depends, HTTPException
+from app.models.user import User
 
 SessionLocal = sessionmaker(bind=db, autoflush=False, autocommit=False)
 
@@ -9,3 +13,19 @@ def get_db():
         yield session
     finally:
         session.close()
+
+
+def verificar_token(token: str = Depends(oauth2_scheme), session: Session = Depends(get_db)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        iduser = int(payload.get("sub"))
+        if iduser is None:
+            raise JWTError()
+        
+    except JWTError:    
+        raise HTTPException(status_code=401, detail="Token inválido")
+    
+    usuario = session.query(User).filter(User.id == iduser).first()
+    if not usuario:
+        raise HTTPException(status_code=401, detail="Acesso invalido") 
+    return usuario      
--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,34 @@
+import os
+from dotenv import load_dotenv
 from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+from jose import JWTError, jwt
+from fastapi.security import OAuth2PasswordBearer 
 
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS"))
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login_formulario")
+
+def hash_senha(password: str):
+    return bcrypt_context.hash(password)
+
+def verificar_senha(password: str, hash: str):
+    return bcrypt_context.verify(password, hash)
+
+def criar_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def criar_refresh_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
