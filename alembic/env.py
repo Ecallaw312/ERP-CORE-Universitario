f8d0430181cccfac65bb9456f0a0162b2b276e09
Ecallaw312 <wallacesouza.130903@gmail.com>
2026-06-01 00:39:21 -0300

    Refactor run_migrations_online to use existing DB engine

--- alembic/env.py
+++ alembic/env.py
@@ -60,23 +60,12 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
+       from app.core.database import db  # usa a engine já configurada com o driver certo
 
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
-    with connectable.connect() as connection:
+    with db.connect() as connection:
         context.configure(
             connection=connection, target_metadata=target_metadata
         )
-
         with context.begin_transaction():
             context.run_migrations()
 
