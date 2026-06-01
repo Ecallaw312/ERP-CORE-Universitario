495c2ab592c9fb70b7d0dcf983fcf1c19697633a
Wallace Souza <wallacesouza.130903@gmail.com>
2026-06-01 00:17:47 -0300

    Ajustem variaves par aproducao

--- app/core/database.py
+++ app/core/database.py
@@ -1,8 +1,11 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
- 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./core.db")
- 
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL and DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg://", 1)
+
 db = create_engine(DATABASE_URL, echo=True, future=True)
 Base = declarative_base()
